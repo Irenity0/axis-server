b1be6b16b5a574b24e4600ef42339869940bf289
Afra Anjum <lilithkard77@gmail.com>
2025-11-11 12:47:10 +0600

    connect database to server

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.get("/", (req, res) => {
     success: true,
     message: "Axis server uwu",
   })
-})
+}) 
 
 // Global error handler
 app.use((error: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
