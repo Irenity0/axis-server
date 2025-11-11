a1c34584d0b883972dea6600cf447daf03edfe42
Afra Anjum <lilithkard77@gmail.com>
2025-11-11 12:58:17 +0600

    added types

---
+++ src/types/index.ts
@@ -0,0 +1,36 @@
+import { Document } from "mongoose";
+
+export interface IUser extends Document {
+  _id: string;
+  email: string;
+  name: string;
+  password: string;
+  createdAt: Date;
+}
+
+export interface ITask extends Document {
+  _id: string;
+  type: "task" | "event";
+  status: "todo" | "in-progress" | "done";
+  title: String;
+  description: String;
+  start: Date;
+  end: Date;
+  allDay: Boolean;
+  email: String;
+  priority: "low" | "medium" | "high";
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface IHabit extends Document {
+  name: String; // e.g., "Workout", "Read 10 pages"
+  email: String; // user identifier
+  month: Number; // e.g., 11 for November
+  year: Number; // e.g., 2025
+  days: {
+    [day: number]: boolean; // maps day (1–31) to completion status
+  };
+  createdAt: Date;
+  updatedAt: Date;
+}
